0e8f76751feb954e2ffe4f778273531a91f26682
Andrewfro <743961@student.dusd.net>
2023-04-03 09:53:07 -0700

    Worked on project

--- Unit3SoundEffect/Assets/Scripts/SpawnManager.cs
+++ Unit3SoundEffect/Assets/Scripts/SpawnManager.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public float startDelay = 2 
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
-    void Update()
+    void SpawnObstacle()
     {
-        
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+       
     }
 }
